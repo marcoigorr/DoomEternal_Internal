052febdaf324b53e419266da1a475cbcc22ce237
marcoigorr <petronio.mrc@gmail.com>
2022-06-17 15:06:49 +0200

    added health hack (F2)

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <TlHelp32.h>
 #include "mem.h"
+#include "offsets.hpp"
+using namespace Offsets;
+
 
 DWORD WINAPI MainThread(HMODULE hModule)
 {
@@ -16,23 +19,33 @@ DWORD WINAPI MainThread(HMODULE hModule)
 
     // Get Module Base Addr
     uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"DOOMEternalx64vk.exe");
+    std::cout << "[+] Module base: " << "0x" << std::uppercase << std::hex << moduleBase << std::endl;
 
-    bool bAmmo = false;    
+    bool ejectDLL = false;
+    bool bAmmo = false, bHealth = false;   
+
+    uintptr_t gMetrics = moduleBase + oGMetrics[0];    
+    uintptr_t playerEnt = moduleBase + oPlayerEnt[0];
+    std::cout << "[+] Game Metrics addr: " << "0x" << std::hex << gMetrics << std::endl;
+    std::cout << "[+] Player Ent base addr: " << "0x" << std::hex << playerEnt << std::endl;
+
 
     // Hack loop 
-    while (true)
+    while (!ejectDLL)
     {
         // Key input
         if (GetAsyncKeyState(VK_INSERT) & 1)
         {
-            break;
+            std::cout << "\n[+] Ejecting DLL..." << std::endl;
+            ejectDLL = true;
         }
 
-        if (GetAsyncKeyState(VK_F1) & 1)
+        // --- Ammo
+        if (GetAsyncKeyState(VK_F1) & 1 || ejectDLL)
         {
             bAmmo = !bAmmo;
-            if (bAmmo) // nop 
-            { 
+            if (bAmmo && !ejectDLL) // nop 
+            {
                 // DOOMEternalx64vk.exe+1D24A03 - 89 7B 40 - mov [rbx+40],edi
                 mem::Nop((BYTE*)(moduleBase + 0x1D24A03), 3);
                 // DOOMEternalx64vk.exe+1D24A31 - 89 7B 40 - mov [rbx+40],edi
@@ -41,28 +54,43 @@ DWORD WINAPI MainThread(HMODULE hModule)
                 mem::Nop((BYTE*)(moduleBase + 0x1583451), 6);
                 // DOOMEternalx64vk.exe+1561BD5 - 89 82 18010000 - mov [rdx+00000118],eax (warn low ammo)
                 mem::Nop((BYTE*)(moduleBase + 0x1561BD5), 6);
+
+                // Combat Shotgun set to 16
+                *(int*)mem::FindDMAAddy(playerEnt, pEnt::CombatShotgun::oAmmo) = 16;
+                // Heavy Rifle set to 60
+                *(int*)mem::FindDMAAddy(playerEnt, pEnt::HeavyRifle::oAmmo) = 60;
+                // Chainsaw charge set to 3
+                *(int*)mem::FindDMAAddy(playerEnt, pEnt::Chainsaw::oCharge) = 3;
             }
-            else // write original code
+            else if (!bAmmo || ejectDLL) // write original code
             {
                 mem::Patch((BYTE*)(moduleBase + 0x1D24A03), (BYTE*)"\x89\x7B\x40", 3);
                 mem::Patch((BYTE*)(moduleBase + 0x1D24A31), (BYTE*)"\x89\x7B\x40", 3);
                 mem::Patch((BYTE*)(moduleBase + 0x1583451), (BYTE*)"\x89\x82\x28\x01\x00\x00", 6);
                 mem::Patch((BYTE*)(moduleBase + 0x1561BD5), (BYTE*)"\x89\x82\x18\x01\x00\x00", 6);
             }
+            Sleep(5);
         }
-        Sleep(5);
+
+        // --- Health
+        if (GetAsyncKeyState(VK_F2) & 1)
+        {
+            bHealth = !bHealth;
+        }
+
+        if (bHealth)
+        {
+            *(int*)mem::FindDMAAddy(playerEnt, pEnt::oHealth) = 1137836032; // = 420
+        }
     }
-    // Cleanup / Eject
+    // Cleanup/Eject
     fclose(f);
     FreeConsole();
     FreeLibraryAndExitThread(hModule, 0);
     return 0;
 }
-
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+    
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
